5e521a5c64b5253a9b64d176c03ba8e945b8cfc2
v8phan <56612604+v8phan@users.noreply.github.com>
2020-02-19 21:33:54 -0600

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Viet Phan',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
+      ],
+      home: {
+        title: 'Hire Me',
+        subTitle: 'PLEASE',
+        text: 'Projects coming soon'
+      },
+      about: {
+        title: 'About Me'
+      },
+      contact: {
+        title: 'Hire me here'
+      }
+    }
+  }
+
   render() {
     return (
-        <div>Hello</div>
+      <Router>
+        <Container className="p-0" fluid={true}>
+
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Viet Phan</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+
+            </Navbar.Collapse>
+          </Navbar>
+
+        </Container>
+      </Router>
     );
   }
 }
