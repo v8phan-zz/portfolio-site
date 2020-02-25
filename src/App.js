6bdf24e40ae2b812932cda34c286d18532d0f534
v8phan <56612604+v8phan@users.noreply.github.com>
2020-02-24 18:12:06 -0600

    created pages and routed them

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import './App.css';
 import Nav from 'react-bootstrap/Nav';
 
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
+
 
 class App extends React.Component {
 
@@ -47,9 +52,12 @@ class App extends React.Component {
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
               </Nav>
-
             </Navbar.Collapse>
           </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.about.title} />} />
 
           <Footer />
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage(props) {
+
+    return (
+        <p>Home Works!</p>
+    )
+
+}
+
+export default HomePage;
