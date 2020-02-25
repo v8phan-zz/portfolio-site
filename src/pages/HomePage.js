fec10e1ae901b341c24fca93320b4599aa7a6824
v8phan <56612604+v8phan@users.noreply.github.com>
2020-02-24 19:09:20 -0600

    hero component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Hero from '../components/Hero';
+
 function HomePage(props) {
 
     return (
-        <p>Home Works!</p>
+        <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
     )
 
 }
